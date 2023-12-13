023f750624ccbb4a43db67e1367d94a2812c524d
matthewjmckinley <matthewjamesmckinley@gmail.com>
2023-12-13 15:59:25 -0800

    Display relevant hierarchy breadcrumb trails on admin item display page

--- Module.php
+++ Module.php
@@ -38,6 +38,94 @@ class Module extends AbstractModule
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
+        $sharedEventManager->attach(
+            'Omeka\Controller\Admin\Item',
+            'view.show.sidebar',
+            [$this, 'addItemHierarchies']
+        );
+    }
 
+    // Add relevant hierarchy breadcrumbs to item display sidebar
+    public function addItemHierarchies(Event $event)
+    {
+        $view = $event->getTarget();
+        $api = $this->getServiceLocator()->get('Omeka\ApiManager');
+        if ($view->item->itemSets()) {
+            echo '<div class="meta-group">';
+            echo '<h4>' . $view->translate('Hierarchies') . '</h4>';
+            foreach ($view->item->itemSets() as $currentItemSet) {
+                $groupings = $api->search('item_hierarchy_grouping', ['item_set' => $currentItemSet->id()])->getContent();
+                $this->buildBreadcrumb($groupings, $currentItemSet);
+            }
+            echo '</div>';
+        }
+    }
+
+    protected function buildBreadcrumb(array $groupings, $currentItemSet)
+    {
+        $api = $this->getServiceLocator()->get('Omeka\ApiManager');
+        static $printedGroupings = [];
+        $iterate = function ($groupings) use ($api, $currentItemSet, &$iterate, &$allGroupings, &$printedGroupings, &$currentHierarchy) {
+            foreach ($groupings as $key => $grouping) {
+                // Continue if grouping has already been printed
+                if (isset($printedGroupings) && in_array($grouping, $printedGroupings)) {
+                    continue;
+                }
+
+                if ($currentHierarchy != $grouping->getHierarchy()) {
+                    echo '<div class="value">';
+                    $currentHierarchy = $grouping->getHierarchy();
+                    $allGroupings = $api->search('item_hierarchy_grouping', ['hierarchy' => $currentHierarchy])->getContent();
+                }
+
+                if ($grouping->getParentGrouping() != 0) {
+                    // $iterate through any groupings with current grouping as child
+                    $parentArray = array_filter($allGroupings, function($parent) use($grouping) {
+                        return $parent->id() == $grouping->getParentGrouping();
+                    });
+                    if (count($parentArray) > 0) {
+                        $iterate($parentArray, $currentItemSet);
+                        break;
+                    }
+                }
+
+                try {
+                    $itemSet = $api->read('item_sets', $grouping->getItemSet())->getContent();
+                } catch (\Exception $e) {
+                    // Print groupings without assigned itemSet
+                    $itemSet = null;
+                    echo $grouping->getLabel();
+                }
+
+                if (!is_null($itemSet)) {
+                    // Bold groupings with current itemSet assigned
+                    if ($grouping->getItemSet()->getId() == $currentItemSet->id()) {
+                        echo "<b>" . $itemSet->link($grouping->getLabel()) . "</b>";
+                    } else {
+                        echo $itemSet->link($grouping->getLabel());
+                    }
+                }
+
+                // Return any groupings with current grouping as parent
+                $childArray = array_filter($allGroupings, function($child) use($grouping) {
+                    return $child->getParentGrouping() == $grouping->id();
+                });
+
+                // Remove already printed groupings from $allGroupings array
+                $allGroupings = array_filter($allGroupings, function($child) use($grouping) {
+                    return $child->id() != $grouping->id();
+                });
+
+                $printedGroupings[] = $grouping;
+
+                if (count($childArray) > 0) {
+                    echo " => ";
+                    $iterate($childArray, $currentItemSet);
+                    continue;
+                }
+                echo '</div>';
+            }
+        };
+        $iterate($groupings, $currentItemSet);
     }
 }
